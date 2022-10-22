f06186f2665f3600891014339f96158f45fc0ba3
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-10-22 11:47:27 -0300

    Update questao2.cpp

--- questao2.cpp
+++ questao2.cpp
@@ -1,39 +1,45 @@
 #include <stdio.h>
-int main (){
-	
+
+int main ()
+{
+  do {
+    
 	int ll, cc;
 	
 	printf("Digite o numero de linhas da matriz: ");
 	scanf("%d", &ll);
-	
 	printf("Digite o numero de colunas da matriz: ");
 	scanf("%d", &cc);
 	
 	int l, c, op, matEnd[ll][cc];
-	
-	printf("\n");
-	
-	for (l=0;l<ll;l++){
-		for(c=0;c<cc;c++){
-			printf("Digite o valor da posicao [%d][%d]:", l, c);
-			scanf("%d", &matEnd[l][c]); } }
-			
-	if (ll==cc) {
+
+	for (l=0;l<ll;l++)
+  {
+		for(c=0;c<cc;c++)
+    {
+			printf("\nDigite o valor da posicao [%d][%d]:", l, c);
+			scanf("%d", &matEnd[l][c]); 
+    } 
+  }
+	if (ll==cc) 
+  {
+		printf("\nA matriz matEnd eh quadrada \n"); 
+    }
+			else 
+  {
+				printf("\nA matriz matEnd nao eh quadrada\n"); 
+    }
 		
-		printf("\nA matriz matEnd eh quadrada \n"); }
-		
-			else {
-				printf("\nA matriz matEnd nao eh quadrada\n"); }
+	printf("\nImpressao da matriz matEnd: \n\n");
 			
-	printf("\n");
-		
-	printf("Impressao da matriz matEnd: \n\n");
-			
-	for (l=0;l<ll;l++){
-		
-		for(c=0;c<cc;c++){
-			
-			printf("%d  ", matEnd[l][c]); }
-			printf("\n"); } 
+	for (l=0;l<ll;l++)
+  {
+		for(c=0;c<cc;c++)
+    {
+			printf("%d  ", matEnd[l][c]); 
+    }
+		printf("\n");
+  } 
+    } while(1);
 	
 	return 0; }
