43ed50fde2c70cce574bf93bb4f7ba82c0db91f0
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-29 10:41:32 -0300

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -1,45 +1,33 @@
 #include <stdio.h>
+#include <locale.h>
 
-int main ()
-{
-  do {
-    
-	int ll, cc;
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int N, op;
+	printf("Digite o tamanho do vetor A: ");
+	scanf("%d", &N);
 	
-	printf("Digite o numero de linhas da matriz: ");
-	scanf("%d", &ll);
-	printf("Digite o numero de colunas da matriz: ");
-	scanf("%d", &cc);
+	int A[N];
 	
-	int l, c, op, matEnd[ll][cc];
-
-	for (l=0;l<ll;l++)
-  {
-		for(c=0;c<cc;c++)
-    {
-			printf("\nDigite o valor da posicao [%d][%d]:", l, c);
-			scanf("%d", &matEnd[l][c]); 
-    } 
-  }
-	if (ll==cc) 
-  {
-		printf("\nA matriz matEnd eh quadrada \n"); 
-  }
-			else 
-  {
-		printf("\nA matriz matEnd nao eh quadrada\n"); 
-  }
-		
-	printf("\nImpressao da matriz matEnd: \n\n");
-			
-	for (l=0;l<ll;l++)
-  {
-		for(c=0;c<cc;c++)
-    {
-			printf("%d  ", matEnd[l][c]); 
-    }
-		printf("\n");
-  } 
-    } while(1);
+	for (int i=0;i<N;i++){
+		printf("Digite o valor da posição %d: ", i);
+		scanf("%d", &A[i]);
+	}
+	printf("1.Imprime o vetor A na tela\n2.Imprime o vetor A na ordem inversa\nDigite sua escolha: ");
+	scanf("%d", &op);
 	
-	return 0; }
+	switch (op){
+		case 1:
+			for(int i =0;i<N;i++){
+				printf("Vetor A posição %d = %d\n", i, A[i]);
+			}
+			break;
+	
+	case 2:
+			for(int i=N-1;i>=0;i--){
+				printf("Vetor A posição %d = %d\n", i, A[i]);
+			}
+			break;
+	}
+	return 0;
+}
